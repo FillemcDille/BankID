2e45d77be9835394fc079cdb3fa61a9d61a25dea
Filip <Filipnordic@gmail.com>
2025-10-16 12:48:37 +0200

    Uppdaterad Transaction

--- BlazorApp4/Domain/Transaction.cs
+++ BlazorApp4/Domain/Transaction.cs
@@ -1,6 +1,21 @@
 ﻿namespace BlazorApp4.Domain
 {
+    public enum TransactionType
+    {
+        Deposit,
+        Withdrawal,
+        Transfer
+    }
     public class Transaction
     {
+        public Guid Id { get; set; }  = Guid.NewGuid();
+        public Guid FromAccountId { get; set; } = Guid.NewGuid();
+        public Guid ToAccountId { get; set; } = Guid.NewGuid();
+        public decimal Amount { get; set; } = new decimal();
+        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+        public TransactionType TransactionType { get; set; } = new TransactionType();
+
+        
     }
+    
 }
